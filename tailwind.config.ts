994c6de81d776575b3c595906bf44d4945bfe7bf
Otto Haarahiltunen <otto.haarahiltunen@tuni.fi>
2024-12-02 14:55:20 +0200

    Siirretään lisätyt värit extend osioon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,16 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
-    colors: {
-      'text': '#021d06',
-      'background': '#f6fef7',
-      'primary': '#04580e',
-      'secondary': '#75b5f5',
-      'accent': '#6058f3',
-      'white':"white"
-     },
+    extend: {
+      colors: {
+        'text': '#021d06',
+        'background': '#f6fef7',
+        'primary': '#04580e',
+        'secondary': '#75b5f5',
+        'accent': '#6058f3',
+        'white':"white"
+       },
+    },
   },
 
   plugins: [typography, forms]
